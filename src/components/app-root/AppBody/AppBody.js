6dad4788455c765350a99060727d61777815d376
Rod Glover <rglover@uvic.ca>
2020-08-31 16:28:17 -0700

    Use react-table for initial display

--- src/components/app-root/AppBody/AppBody.js
+++ src/components/app-root/AppBody/AppBody.js
@@ -1,53 +1,82 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import map from 'lodash/fp/map';
 import locations from '../../../assets/locations';
+import { useTable } from 'react-table';
 
 
-export default class AppBody extends React.Component {
-  static propTypes = {
-  };
+export default function AppBody() {
+  const data = React.useMemo(() => locations, []);
 
-  state = {
-  };
+  const columns = React.useMemo(
+    () => [
+      { Header: "City", accessor: "city" },
+      { Header: "Province", accessor: "province" },
+      { Header: "Code", accessor: "code" },
+      { Header: "Latitude", accessor: "latitude" },
+      { Header: "Longitude", accessor: "longitude" },
+      { Header: "Elevation", accessor: "elevation" },
+    ],
+    []
+  );
 
-  render() {
-    return (
-      <Row>
-        <Col lg={12}>
-          <ul>
-            {
-              map(loc => (
-                <li>
-                  <div>
-                    Name: {loc.name}, {' '}
-                    Code: {loc.code}, {' '}
-                    Lat: {loc.latitude}, {' '}
-                    Lon: {loc.longitude}
-                  </div>
-                  Files:
-                  <ul>
-                    {
-                      map(file => (
-                        <li>
-                          Type: {file.fileType}, {' '}
-                          {
-                            file.fileType === "weather" ?
-                              file.timePeriod.start :
-                              "..."
-                          }
-                        </li>
-                      ))(loc.files)
-                    }
-                  </ul>
-                </li>
-              ))(locations)
-            }
-          </ul>
-        </Col>
-      </Row>
-    );
-  }
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+
+  return (
+    <Row>
+      <Col lg={12}>
+        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+          <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    borderBottom: 'solid 3px red',
+                    background: 'aliceblue',
+                    color: 'black',
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '10px',
+                        border: 'solid 1px gray',
+                        background: 'papayawhip',
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+      </Col>
+    </Row>
+  );
 }
