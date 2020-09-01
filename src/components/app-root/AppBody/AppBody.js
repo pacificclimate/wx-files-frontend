3934462cfb0913a877a4c5b68316200577991311
Rod Glover <rglover@uvic.ca>
2020-09-01 14:24:32 -0700

    Add ultrasimple row filtering

--- src/components/app-root/AppBody/AppBody.js
+++ src/components/app-root/AppBody/AppBody.js
@@ -2,15 +2,41 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import locations from '../../../assets/locations';
-import { useTable, useSortBy } from 'react-table';
+import { useTable, useSortBy, useFilters } from 'react-table';
+
+
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  );
+}
+
 
 
 export default function AppBody() {
   const data = React.useMemo(() => locations, []);
 
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  );
+
   const columns = React.useMemo(
     () => [
-      { Header: "City", accessor: "city" },
+      { Header: "City", accessor: "city", filter: 'text' },
       { Header: "Province", accessor: "province" },
       { Header: "Code", accessor: "code" },
       { Header: "Latitude", accessor: "latitude" },
@@ -26,7 +52,11 @@ export default function AppBody() {
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({ columns, data }, useSortBy);
+  } = useTable(
+    { columns, data, defaultColumn },
+    useFilters,
+    useSortBy,
+  );
 
   return (
     <Row>
@@ -45,7 +75,7 @@ export default function AppBody() {
                     fontWeight: 'bold',
                   }}
                 >
-                  {column.render('Header')}
+                  {column.render('Header' )}
                   <span>
                     {column.isSorted
                       ? column.isSortedDesc
@@ -53,6 +83,7 @@ export default function AppBody() {
                         : ' 🔼'
                       : ''}
                   </span>
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
                 </th>
               ))}
             </tr>
