11873690f0910adcae019cd80aed2290af2cfde4
Rod Glover <rglover@uvic.ca>
2020-09-02 10:41:27 -0700

    Add and use different coordinates filter

--- src/components/app-root/AppBody/AppBody.js
+++ src/components/app-root/AppBody/AppBody.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import locations from '../../../assets/locations';
-import { useTable, useSortBy, useFilters } from 'react-table';
+import { useFilters, useSortBy, useTable } from 'react-table';
 
 
 // Custom filter UIs
@@ -176,8 +176,9 @@ function textStartsWith(rows, id, filterValue) {
 }
 textStartsWith.autoRemove = val => !val;
 
+// Custom filter for coordinates within a bounding box defined in km.
 function coordinatesInBox(rows, id, filterValue) {
-  const [latFilter, lonFilter, distance] = filterValue;
+  const [latCentre, lonCentre, distance] = filterValue;
 
   const bounds = (center, tolerance) =>
     [center - tolerance, center + tolerance];
@@ -187,31 +188,55 @@ function coordinatesInBox(rows, id, filterValue) {
   // approx km / deg lat at Canadian latitudes
   // (varies by < 1% from equator to pole, according to ellipsoid)
   const latTolerance = distance / 111.2;
-  const latBounds = bounds(latFilter, latTolerance);
+  const latBounds = bounds(latCentre, latTolerance);
 
   // approx km / deg lon at specified latitude; if latitude unspecified, use
   // a default value of 55.
   const lonTolerance =
-    distance / (111.32 * Math.cos((latFilter || 55.0) / 180 * Math.PI));
-  const lonBounds = bounds(lonFilter, lonTolerance);
+    distance / (111.32 * Math.cos((latCentre || 55.0) / 180 * Math.PI));
+  const lonBounds = bounds(lonCentre, lonTolerance);
 
-  const result = rows.filter(row => {
+  return rows.filter(row => {
     const [latitude, longitude] = row.values[id];
-    const pred = (
-      (!latFilter || inBounds(latitude, latBounds)) &&
-      (!lonFilter || inBounds(longitude, lonBounds))
+    return (
+      (!latCentre || inBounds(latitude, latBounds)) &&
+      (!lonCentre || inBounds(longitude, lonBounds))
     );
-    console.log("### lat, lon, pred", latitude, longitude, pred)
-    return pred;
   });
-  console.groupEnd()
-  return result;
 }
 coordinatesInBox.autoRemove = val =>
   !val || (typeof val[0] !== 'number' && typeof val[1] !== 'number' &&
   typeof val[2] !== 'number');
 
+// Custom filter for coordinates within a radius in km.
+function coordinatesWithinRadius(rows, id, filterValue) {
+  const [latCentre, lonCentre, distance] = filterValue;
 
+  const dist = (lat1, lon1, lat2, lon2) => {
+    // Compute distance between two lat-lon points using haversine formula.
+    // Courtesy of http://www.movable-type.co.uk/scripts/latlong.html .
+    // Cool unknown thing: Many non-ascii characters are legit in identifiers.
+    const R = 6371; // km
+    const d2R = Math.PI / 180;
+    const φ1 = lat1 * d2R;
+    const φ2 = lat2 * d2R;
+    const Δφ = (lat2-lat1) * d2R;
+    const Δλ = (lon2-lon1) * d2R;
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+      Math.cos(φ1) * Math.cos(φ2) *
+      Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return R * c;
+  }
+
+  return rows.filter(row => {
+    const [latitude, longitude] = row.values[id];
+    return dist(latCentre, lonCentre, latitude, longitude) < distance;
+  });
+}
+coordinatesWithinRadius.autoRemove = val =>
+  !val || (typeof val[0] !== 'number' && typeof val[1] !== 'number' &&
+  typeof val[2] !== 'number');
 
 
 export default function AppBody() {
@@ -227,6 +252,7 @@ export default function AppBody() {
     () => ({
       textStartsWith,
       coordinatesInBox,
+      coordinatesWithinRadius,
     }),
     []
   );
@@ -260,7 +286,7 @@ export default function AppBody() {
         // id: "coordinates",
         accessor: "coordinates",
         Filter: CoordinatesNearColumnFilter,
-        filter: "coordinatesInBox",
+        filter: "coordinatesWithinRadius",
       },
       // {
       //   Header: "Latitude",
