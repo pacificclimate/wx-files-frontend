dd0904ec8f08a72273a2339870da1c6760b703a7
rod-glover <rod.glover.ca@gmail.com>
2020-09-08 09:34:59 -0700

    Use stylesheet

--- src/components/data-grid/DataGrid/DataGrid.js
+++ src/components/data-grid/DataGrid/DataGrid.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import {
   coordinatesInBox, coordinatesWithinRadius, textStartsWith
 } from '../column-filters/filterTypes';
 import DefaultColumnFilter from '../column-filters/DefaultColumnFilter';
 import { useTable, useFilters, useSortBy, useExpanded } from 'react-table';
+import styles from './DataGrid.module.css';
 
 
 export function DataGrid({ columns, data, renderRowExpansion}) {
@@ -41,10 +40,6 @@ export function DataGrid({ columns, data, renderRowExpansion}) {
     useExpanded,
   );
 
-  const makeHandleClickRow = row => () => {
-    setFilter("coordinates", [...row.values.coordinates, 100]);
-  }
-
   const makeHandleClickCell = cell => () => {
     switch(cell.column.id) {
       case "coordinates":
@@ -57,76 +52,62 @@ export function DataGrid({ columns, data, renderRowExpansion}) {
   }
 
   return (
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-      <thead>
-      {headerGroups.map(headerGroup => (
-        <>
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                style={{
-                  background: 'aliceblue',
-                  color: 'black',
-                  fontWeight: 'bold',
-                }}
-              >
-                {column.render('Header')}
-                <span>
-                {column.isSorted
-                  ? column.isSortedDesc
-                    ? ' 🔽'
-                    : ' 🔼'
-                  : ''}
-              </span>
-              </th>
-            ))}
-          </tr>
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderBottom: 'solid 3px red',
-                  background: 'aliceblue',
-                  color: 'black',
-                  fontWeight: 'bold',
-                }}
-              >
-                <div>{column.canFilter ? column.render('Filter') : null}</div>
-              </th>
-            ))}
-          </tr>
-        </>
-      ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-      {rows.map(row => {
-        prepareRow(row)
-        return (
-          <React.Fragment {...row.getRowProps()} >
-            <tr>
-              {row.cells.map(cell => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: '10px',
-                      border: 'solid 1px gray',
-                      background: 'papayawhip',
-                    }}
-                    onClick={makeHandleClickCell(cell)}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
+    <div className={styles.DataGrid}>
+      <table
+        {...getTableProps()}
+      >
+        <thead>
+        {headerGroups.map(headerGroup => (
+          <>
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
+                  {column.render('Header')}
+                  <span>
+                  {column.isSorted
+                    ? column.isSortedDesc
+                      ? ' 🔽'
+                      : ' 🔼'
+                    : ''}
+                </span>
+                </th>
+              ))}
             </tr>
-            {row.isExpanded ? renderRowExpansion({ row, visibleColumns }) : null}
-          </React.Fragment>
-        )
-      })}
-      </tbody>
-    </table>
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
+              ))}
+            </tr>
+          </>
+        ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row)
+          return (
+            <React.Fragment {...row.getRowProps()} >
+              <tr>
+                {row.cells.map(cell => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      onClick={makeHandleClickCell(cell)}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+              {row.isExpanded ? renderRowExpansion({ row, visibleColumns }) : null}
+            </React.Fragment>
+          )
+        })}
+        </tbody>
+      </table>
+    </div>
   );
 }
