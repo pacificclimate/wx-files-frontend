9b92b99fa018c12f7604e9f18cc7d98f4fa6b96c
rod-glover <rod.glover.ca@gmail.com>
2020-09-23 16:11:20 -0700

    Remove unnecessary placeholder content

--- src/components/data-grid/column-filters/NumberRangeColumnFilter/NumberRangeColumnFilter.js
+++ src/components/data-grid/column-filters/NumberRangeColumnFilter/NumberRangeColumnFilter.js
@@ -4,18 +4,8 @@ import { makeFilterNumberInputOnChange } from '../../../../utils/filters';
 // Custom filter UI for selecting number within a range (min, max).
 
 export default function NumberRangeColumnFilter({
- column: { filterValue = [], preFilteredRows, setFilter, id },
+ column: { filterValue = [], setFilter, id },
 }) {
-  const [min, max] = React.useMemo(() => {
-    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
-    preFilteredRows.forEach(row => {
-      min = Math.min(row.values[id], min)
-      max = Math.max(row.values[id], max)
-    })
-    return [min, max]
-  }, [id, preFilteredRows])
-
   return (
     <div
       style={{
@@ -26,7 +16,7 @@ export default function NumberRangeColumnFilter({
         value={filterValue[0] || ''}
         type="number"
         onChange={makeFilterNumberInputOnChange(setFilter, 0)}
-        placeholder={`Min (${min})`}
+        placeholder={'Min'}
         style={{
           width: '70px',
           marginRight: '0.5rem',
@@ -37,7 +27,7 @@ export default function NumberRangeColumnFilter({
         value={filterValue[1] || ''}
         type="number"
         onChange={makeFilterNumberInputOnChange(setFilter, 1)}
-        placeholder={`Max (${max})`}
+        placeholder={'Max'}
         style={{
           width: '70px',
           marginLeft: '0.5rem',
@@ -46,7 +36,7 @@ export default function NumberRangeColumnFilter({
       />
       <button onClick={() => setFilter(undefined)}>Clear</button>
     </div>
-  )
+  );
 }
 
 
