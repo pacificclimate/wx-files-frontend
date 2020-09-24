207d83bffbe339c4578682bdbddef673454b8bf0
rod-glover <rod.glover.ca@gmail.com>
2020-09-24 13:52:08 -0700

    Update component Template

--- src/components/0Template/Template.js
+++ src/components/0Template/Template.js
@@ -2,16 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 
-export default class Template extends React.Component {
-  static propTypes = {
-  };
-
-  state = {
-  };
-
-  render() {
-    return (
-      <div>Template</div>
-    );
-  }
+export default function Template() {
+  return (
+    <div>Template</div>
+  );
 }
