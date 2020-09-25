b48151e7818aae9bd1df653ff21776d7e5906b28
rod-glover <rod.glover.ca@gmail.com>
2020-09-25 15:19:08 -0700

    Render Note/Notes components more sedately

--- src/components/notes/Notes/Notes.js
+++ src/components/notes/Notes/Notes.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Cards from 'react-bootstrap/CardColumns';
-
 
 export default function Notes({ children }) {
   return (
-    <Cards>{children}</Cards>
+    <ul>{children}</ul>
   );
 }
