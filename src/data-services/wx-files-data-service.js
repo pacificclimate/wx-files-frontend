8602b0efc99712bac79a4af7a8ce7872233e9989
rod-glover <rod.glover.ca@gmail.com>
2020-09-28 11:59:25 -0700

    Add fake, immediate data service

---
+++ src/data-services/wx-files-data-service.js
@@ -0,0 +1,8 @@
+// import axios from 'axios';
+// import urljoin from 'url-join';
+import locations from '../assets/locations';
+
+
+export function fetchWxFilesMetadata() {
+  return Promise.resolve(locations);
+}
