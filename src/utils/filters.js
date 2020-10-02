c60d17325cffa301d9b51d7b6f10e090c217bfff
rod-glover <rod.glover.ca@gmail.com>
2020-10-02 10:10:23 -0700

    Fix makeFilterNumberInputOnChange

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -2,11 +2,12 @@ import { mapWithKey } from './lodash-fp-extras';
 
 
 // Abstraction of a common numeric filter input UI onChange function.
-export const makeFilterNumberInputOnChange = (setFilter, index) => e => {
-  const val = e.target.value
-  setFilter(
-    (prevFilterValues = []) => mapWithKey(
-      (prevFilterValue, i) => i === index ? val && Number(val) : prevFilterValue
-    )(prevFilterValues)
-  );
+export const makeFilterNumberInputOnChange =
+  ({ filterValue: prevFilterValues, setFilter }, index) => e =>
+{
+  const inputValue = e.target.value;
+  const nextFilterValues = mapWithKey((prevFilterValue, i) =>
+    i === index ? inputValue && Number(inputValue) : prevFilterValue
+  )(prevFilterValues);
+  setFilter(nextFilterValues);
 };
