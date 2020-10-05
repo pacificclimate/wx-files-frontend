39695eac24f004917475b1cab461d449e43d1ca3
rod-glover <rod.glover.ca@gmail.com>
2020-10-05 11:29:54 -0700

    Improve app header

--- src/components/app-root/AppHeader/AppHeader.js
+++ src/components/app-root/AppHeader/AppHeader.js
@@ -1,14 +1,15 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import pcicLogo from '../../../assets/pcic-logo.png';
+import wxFilesLogo from '../../../assets/wx-files-logo.png';
+import styles from './AppHeader.module.css';
 
 export default function AppHeader() {
     return (
-      <Row className={'AppHeader justify-content-center'}>
+      <Row className={`${styles.AppHeader} justify-content-center`}>
         <Col lg={'auto'} md={'auto'} sm={'auto'}>
-          <a href='https://pacificclimate.org/' className={'logo'}>
+          <a href='https://pacificclimate.org/' className={styles.logo}>
             <img
               src={pcicLogo}
               width='328'
@@ -17,7 +18,10 @@ export default function AppHeader() {
             />
           </a>
           <a href='#'>
-            Wx Files logo here
+            <img
+              src={wxFilesLogo}
+              alt={'Wx Files'}
+            />
           </a>
         </Col>
       </Row>
